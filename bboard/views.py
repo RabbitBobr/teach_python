4ff1ee9737313e3d00010080a8bdedf3ed8a69d5
RabbitBobr <RabbitBobr@gmail.com>
2020-05-05 00:43:00 +0300

    day one, edited

--- bboard/views.py
+++ bboard/views.py
@@ -24,7 +24,7 @@ def by_rubric(request, rubric_id):
 class BbCreateView(CreateView):
     template_name = 'bboard/create.html'
     form_class = BbForm
-    success_url = reverse_lazy('index')
+    success_url = '/bboard/'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
